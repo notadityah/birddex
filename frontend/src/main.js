ed9569ab1aa3992f8872b7fa52fe01cd004b6827
adih149 <aditya12320@gmail.com>
2026-02-17 15:03:55 +1100

    Revert "fix: update Amplify configuration to use dynamic import for outputs"
    This reverts commit 1f9fb6cd731acfcf4c63988419cca83126558f0f.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,12 @@
 import './assets/main.css'
 
 import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const outputs = await import('../amplify_outputs.json')
-Amplify.configure(outputs.default)
+Amplify.configure(outputs)
 
 const app = createApp(App)
 
